ba1d8226fac7a2c4a2de9609a575e01eb27637af
unknown <IFSC@CDR-750878.cacador.ifsc.edu.br>
2019-09-20 21:55:00 -0300

    Incluido a função Incluir Usuario

--- models/usuario.php
+++ models/usuario.php
@@ -10,22 +10,25 @@ class Usuario{
 
     public function logar($login, $senha){
         $conexaoDB = $this->conectarBanco();
-        $sql = $conexapDB->prepara("select nomecompleto, email, login from usuario
+        $sql = $conexaoDB->prepara("select nomecompleto, email, login from usuario
                                     where
                                     login = ?
                                     and
                                     senha = ?");
-    $sql->bind_param("ss", $login, $senha);
-    $sql>execute();
+        $sql->bind_param("ss", $login, $senha);
+        //String String -> ss
+        //Int Int -> ii
+        $sql>execute();
 
-    $resultado = $sql->get_result();
-        if($resultado->num_rows() == "0"){
-        $this->nomecompleto = null;
-        $this->email = null;
-        $this->login = null;
-        $this->estaLogado = FALSE;
+        $resultado = $sql->get_result(); //Filtra o resultado em uma linha
+        if($resultado->num_rows === 0){ //Número de linhas = 0 -> Login Incorreto
+            $this->nomecompleto = null;
+            $this->email = null;
+            $this->login = null;
+            $this->estaLogado = FALSE;
         }else{
-            While($linha = $resultado->fetch_assoc()){
+            //Login certo
+            while($linha = $resultado->fetch_assoc()){ //Cada resulttadp lê uma linhas
                 $this->login = $linha['login'];
                 $this->nomecompleto = $linha['nomecompleto'];
                 $this->email = $linha['email'];
@@ -33,13 +36,30 @@ class Usuario{
             }
         }
         $sql->close();
+        $conexaoDB->close();
+        return $this->estaLogado;
     }
+    public function incluirUsuario($nomecompleto, $email, $loin, $senha){
+        $conexaoDB = $this->conectarBanco();
 
+        $sqlInsert = $conexaoDB->prepare("insert into usuario
+                                        (nomecompleto, email, login, senha
+                                        values
+                                        (?, ?, ?, ?)");
+
+        $sqlInsert->bind_param("ssss", $nomecompleto, $email, $loin, $senha);
+
+        $sqlInsert->execute();
+
+        return TRUE;
+    }
     private function conectarBanco(){
         $conn = new \mysqli('localhost', 'root', '', 'cadastrodeprospect');
         return $conn;
     }
 }
+
+
 
 
 ?>
